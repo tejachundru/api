fa45b24367d4b9a83d6c3f2dacc564f0aec8c871
Jun <junaidvayoli@gmail.com>
2020-06-12 00:55:48 +0900

    add district testing data

--- src/sheet-to-json_generic.js
+++ src/sheet-to-json_generic.js
@@ -145,5 +145,11 @@ const c = require("../lib/constants");
     file: c.FILE_ZONES
   });
 
+  await task({
+    sheet: c.SHEET,
+    tabs: { district_testing_data: c.SHEET_DISTRICT_TESTING_DATA },
+    file: c.FILE_DISTRICT_TESTING_DATA
+  });
+
   console.log("End of sheet-to-json_generic");
 })();
